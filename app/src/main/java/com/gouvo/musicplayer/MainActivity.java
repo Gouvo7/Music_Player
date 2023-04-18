8260a5f8400bf0640192737e69da8f5ef676ddef
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2023-04-18 15:55:02 +0300

    Loading files functionality added

--- app/src/main/java/com/gouvo/musicplayer/MainActivity.java
+++ app/src/main/java/com/gouvo/musicplayer/MainActivity.java
@@ -5,32 +5,64 @@ import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
 import android.os.Bundle;
+import android.content.ClipData;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import java.io.File;
 
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button select;
-
+    Button selectFilesButton;
+    private ActivityResultLauncher<Intent> fileSelectorLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        select = (Button) findViewById (R.id.select);
-
-        select.setOnClickListener (new View.OnClickListener ()
-        {
-            @Override
-            public void onClick (View v)
-            {
-                Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
-                chooseFile.setType("*/*");
-                chooseFile = Intent.createChooser(chooseFile, "Choose a file");
-                // lets hope this passes startActivityForResult(chooseFile, );
+        selectFilesButton = findViewById(R.id.select);
+        fileSelectorLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+                if (data != null) {
+                    ClipData clipData = data.getClipData();
+                    if (clipData != null) {
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            Uri uri = clipData.getItemAt(i).getUri();
+                            loadFileWithThumbnail(uri);
+                        }
+                    } else {
+                        Uri uri = data.getData();
+                        loadFileWithThumbnail(uri);
+                    }
+                }
             }
         });
+        selectFilesButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.setType("audio/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            fileSelectorLauncher.launch(intent);
+        });
+    }
+    private void loadFileWithThumbnail(Uri uri) {
+        String filePath = uri.getPath();
+        File file = new File(filePath);
+        Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(filePath, MediaStore.Images.Thumbnails.MINI_KIND);
+        ImageView imageView = new ImageView(this);
+        imageView.setImageBitmap(thumbnail);
+        LinearLayout imageContainer = findViewById(R.id.image_container);
+        imageContainer.addView(imageView);
     }
 
     @Override
