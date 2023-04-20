070811df6332ab54e7d2ffe558065061de7f8e4a
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2023-04-20 16:15:11 +0300

    Showing filenames and scrolling correctly, need to add thumbnail image.

--- app/src/main/java/com/gouvo/musicplayer/MainActivity.java
+++ app/src/main/java/com/gouvo/musicplayer/MainActivity.java
@@ -2,29 +2,31 @@ package com.gouvo.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.util.Log;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.content.ClipData;
-import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import androidx.activity.result.ActivityResult;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button selectFilesButton;
+    Button clearButton;
     private ActivityResultLauncher<Intent> fileSelectorLauncher;
     private List<String> loadedFiles = new ArrayList<>();
     @Override
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         selectFilesButton = findViewById(R.id.select);
+        clearButton = findViewById(R.id.clear);
         fileSelectorLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == RESULT_OK) {
                 Intent data = result.getData();
@@ -56,42 +59,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
             fileSelectorLauncher.launch(intent);
         });
+
+        clearButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                LinearLayout imageContainer = findViewById(R.id.image_container);
+                imageContainer.removeAllViews();
+                loadedFiles.clear();
+            }
+        });
     }
     private void loadFileWithThumbnail(Uri uri) {
         String filePath = uri.getPath();
         File file = new File(filePath);
         String fileName = file.getName();
-
         if (loadedFiles.contains(fileName)) {
-            Log.d("MyApp", "File already loaded: " + fileName);
+            //Log.d("MyApp", "File already loaded: " + fileName);
             return;
         }
 
-
         loadedFiles.add(file.getName());
-
-        System.out.println(file.getName());
-
-        Log.d("MyApp", "Loaded file: " + file.getName());
-        Log.d("MyApp", "Loaded files so far: " + loadedFiles);
-
+        System.out.println("File path is:" + filePath);
+        Bitmap img = coverpicture(filePath);
         LinearLayout imageContainer = findViewById(R.id.image_container);
+        imageContainer.removeAllViews();
+
         for (String loadedFile : loadedFiles) {
             TextView textView = new TextView(this);
             textView.setText(loadedFile);
-
+            ImageView img_tst = findViewById(R.id.thumbnail);
+            img_tst.setImageBitmap(img);
             LinearLayout linearLayout = new LinearLayout(this);
             linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             linearLayout.setOrientation(LinearLayout.HORIZONTAL);
             linearLayout.addView(textView);
+            linearLayout.addView(img_tst);
 
             imageContainer.addView(linearLayout);
         }
     }
+    public Bitmap coverpicture(String path) {
 
+        System.out.println("Path is!: "+ path);
+        MediaMetadataRetriever mr = new MediaMetadataRetriever();
+        System.out.println(path);
+        mr.setDataSource(path);
+
+        byte[] byte1 = mr.getEmbeddedPicture();
+        try {
+            mr.release();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(byte1 != null)
+            return BitmapFactory.decodeByteArray(byte1, 0, byte1.length);
+        else
+            return  null;
+
+    }
     @Override
     public void onClick (View v)
     {
-        System.out.println("Mprabo malaka");
+
     }
 }
