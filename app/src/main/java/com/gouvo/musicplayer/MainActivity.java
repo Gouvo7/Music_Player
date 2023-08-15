dc95569dbaf3790a4dcec0d80b9728175faec797
Nektarios <gouvousisnektarios@gmail.com>
2023-08-15 15:22:39 +0300

    Loading of songs working, but no thumbnails yet.

--- app/src/main/java/com/gouvo/musicplayer/MainActivity.java
+++ app/src/main/java/com/gouvo/musicplayer/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
-import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,12 +13,15 @@ import android.os.Bundle;
 import android.content.ClipData;
 import android.net.Uri;
 import android.widget.LinearLayout;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
-
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,19 +31,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button clearButton;
     private ActivityResultLauncher<Intent> fileSelectorLauncher;
     private List<String> loadedFiles = new ArrayList<>();
+    private static final int PERMISSION_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("1");
+        // Check if permission is already granted
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Request permissions if not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    },
+                    PERMISSION_REQUEST_CODE);
+        }
 
         selectFilesButton = findViewById(R.id.select);
         clearButton = findViewById(R.id.clear);
+
         fileSelectorLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == RESULT_OK) {
                 Intent data = result.getData();
+
                 if (data != null) {
                     ClipData clipData = data.getClipData();
                     if (clipData != null) {
+
                         for (int i = 0; i < clipData.getItemCount(); i++) {
                             Uri uri = clipData.getItemAt(i).getUri();
                             loadFileWithThumbnail(uri);
@@ -60,8 +77,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             fileSelectorLauncher.launch(intent);
         });
 
-        clearButton.setOnClickListener( new View.OnClickListener() {
-
+        clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LinearLayout imageContainer = findViewById(R.id.image_container);
@@ -70,52 +86,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permissions granted, you can now proceed with your app logic
+            } else {
+                // Permissions denied, handle this case (e.g., show a message)
+            }
+        }
+    }
+
     private void loadFileWithThumbnail(Uri uri) {
         String filePath = uri.getPath();
         File file = new File(filePath);
         String fileName = file.getName();
         if (loadedFiles.contains(fileName)) {
-            //Log.d("MyApp", "File already loaded: " + fileName);
             return;
         }
 
         loadedFiles.add(file.getName());
-        System.out.println("File path is:" + filePath);
-        Bitmap img = coverpicture(filePath);
+        Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
+        // Create a new LinearLayout to hold both the text and the image
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        TextView textView = new TextView(this);
+        textView.setText(fileName);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageBitmap(img);
+
+        linearLayout.addView(textView);
+        linearLayout.addView(imageView);
+
         LinearLayout imageContainer = findViewById(R.id.image_container);
-        imageContainer.removeAllViews();
-
-        for (String loadedFile : loadedFiles) {
-            TextView textView = new TextView(this);
-            textView.setText(loadedFile);
-            ImageView img_tst = findViewById(R.id.thumbnail);
-            img_tst.setImageBitmap(img);
-            LinearLayout linearLayout = new LinearLayout(this);
-            linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-            linearLayout.addView(textView);
-            linearLayout.addView(img_tst);
-
-            imageContainer.addView(linearLayout);
-        }
+        imageContainer.addView(linearLayout);
     }
     public Bitmap coverpicture(String path) {
 
         System.out.println("Path is!: "+ path);
         MediaMetadataRetriever mr = new MediaMetadataRetriever();
-        System.out.println(path);
         mr.setDataSource(path);
 
         byte[] byte1 = mr.getEmbeddedPicture();
-        try {
-            mr.release();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if(byte1 != null)
+        // Don't release the MediaMetadataRetriever here
+        // try {
+        //     mr.release();
+        // } catch (IOException e) {
+        //     throw new RuntimeException(e);
+        // }
+        if (byte1 != null) {
             return BitmapFactory.decodeByteArray(byte1, 0, byte1.length);
-        else
-            return  null;
+        } else {
+            return null;
+        }
 
     }
     @Override
