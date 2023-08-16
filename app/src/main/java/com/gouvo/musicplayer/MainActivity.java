ef7e6f3e6673154109b70cbda33abc39bec7e4c1
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2023-08-16 16:51:02 +0300

    Version 2.0

--- app/src/main/java/com/gouvo/musicplayer/MainActivity.java
+++ app/src/main/java/com/gouvo/musicplayer/MainActivity.java
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=1D1Jo1sLBMo
 package com.gouvo.musicplayer;
 import android.provider.MediaStore;
 import android.database.Cursor;
@@ -16,12 +17,16 @@ import android.net.Uri;
 import android.widget.LinearLayout;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,45 +41,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final int PERMISSION_REQUEST_CODE = 1;
 
+
+    RecyclerView mediaView;
+    TextView noMusicTextView;
+
+    boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    void requestPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this, "Please allow the app to read through your music files in order to continue.",Toast.LENGTH_SHORT);
+        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("1");
-        // Check if permission is already granted
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Request permissions if not granted
-            ActivityCompat.requestPermissions(this,
-                    new String[]{
-                            Manifest.permission.READ_EXTERNAL_STORAGE
-                    },
-                    PERMISSION_REQUEST_CODE);
+        mediaView = findViewById(R.id.media_viewer);
+        noMusicTextView = findViewById(R.id.no_songs_text);
+        if (checkPermission() == false){
+            requestPermission();
+            return;
+        }
+
+        String[] projection ={
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,null);
+        while (cursor.moveToNext()){
+            Song song = new Song(cursor.getString(1),cursor.getString(0),cursor.getString(2));
+            if (new File(song.getPath()).exists())
+                songList.add(song);
+        }
+
+        if (songList.size() == 0){
+            noMusicTextView.setVisibility(View.VISIBLE);
+        }else{
+            mediaView.setLayoutManager(new LinearLayoutManager(this));
+            mediaView.setAdapter(new MusicListAdapter(songList, getApplicationContext()));
         }
 
         selectFilesButton = findViewById(R.id.select);
         clearButton = findViewById(R.id.clear);
 
-        fileSelectorLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            if (result.getResultCode() == RESULT_OK) {
-                Intent data = result.getData();
-
-                if (data != null) {
-                    ClipData clipData = data.getClipData();
-                    if (clipData != null) {
-
-                        for (int i = 0; i < clipData.getItemCount(); i++) {
-                            Uri uri = clipData.getItemAt(i).getUri();
-                            loadFileWithThumbnail(uri);
-                        }
-                    } else {
-                        Uri uri = data.getData();
-
-                        loadFileWithThumbnail(uri);
-                    }
-                }
-            }
-        });
         selectFilesButton.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             intent.setType("audio/*");
@@ -85,9 +106,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LinearLayout imageContainer = findViewById(R.id.image_container);
-                imageContainer.removeAllViews();
-                loadedFiles.clear();
+                //LinearLayout imageContainer = findViewById(R.id.image_container);
+                //imageContainer.removeAllViews();
+                //loadedFiles.clear();
             }
         });
         String[] songData = {
@@ -95,19 +116,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 MediaStore.Audio.Media.TITLE,
                 MediaStore.Audio.Media.DURATION
         };
-
-        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
-        // gets all audio related files
-        // for each file content resolver gets audio file data based on sondData array(path, title and duration)
-        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, songData, selection, null, null);
-
-        while(cursor.moveToNext()) {
-            Song song = new Song(cursor.getString(0), cursor.getString(1), cursor.getString(2));
-            if(new File(song.getPath()).exists())
-                songList.add(song);
-        }
     }
-
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -120,93 +129,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /*
-    private void loadFileWithThumbnail(Uri uri) {
-        String filePath = uri.getPath();
-        File file = new File(filePath);
-        String fileName = file.getName();
-        if (loadedFiles.contains(fileName)) {
-            return;
-        }
-
-        loadedFiles.add(file.getName());
-        Bitmap thumbnailBitmap = coverpicture(filePath);
-
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-        TextView textView = new TextView(this);
-        textView.setText(fileName);
-
-        ImageView imageView = new ImageView(this);
-        if (thumbnailBitmap != null) {
-            imageView.setImageBitmap(thumbnailBitmap);
-        } else {
-            //imageView.setImageResource(R.drawable.ic_launcher_background); // Set a default image if thumbnail is not available
-        }
-
-        linearLayout.addView(textView);
-        linearLayout.addView(imageView);
-
-        LinearLayout imageContainer = findViewById(R.id.image_container);
-        imageContainer.addView(linearLayout);
-    }
-    */
-
-    private void loadFileWithThumbnail(Uri uri) {
-        String filePath = uri.getPath();
-        File file = new File(filePath);
-        String fileName = file.getName();
-        if (loadedFiles.contains(fileName)) {
-            return;
-        }
-
-        loadedFiles.add(file.getName());
-        Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
-        // Create a new LinearLayout to hold both the text and the image
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-        TextView textView = new TextView(this);
-        textView.setText(fileName);
-
-        ImageView imageView = new ImageView(this);
-        imageView.setImageBitmap(img);
-
-        if (img != null) {
-            imageView.setImageBitmap(img);
-        } else {
-            imageView.setImageResource(R.drawable.default_thumbnail); // Set a default image if thumbnail is not available
-        }
-
-        linearLayout.addView(textView);
-        linearLayout.addView(imageView);
-
-        LinearLayout imageContainer = findViewById(R.id.image_container);
-        imageContainer.addView(linearLayout);
-    }
-    public Bitmap coverpicture(String path) {
-
-        System.out.println("Path is!: "+ path);
-        MediaMetadataRetriever mr = new MediaMetadataRetriever();
-        mr.setDataSource(path);
-
-        byte[] byte1 = mr.getEmbeddedPicture();
-        // Don't release the MediaMetadataRetriever here
-        // try {
-        //     mr.release();
-        // } catch (IOException e) {
-        //     throw new RuntimeException(e);
-        // }
-        if (byte1 != null) {
-            return BitmapFactory.decodeByteArray(byte1, 0, byte1.length);
-        } else {
-            return null;
-        }
-
-    }
     @Override
     public void onClick (View v)
     {
---
+++ app/src/main/java/com/gouvo/musicplayer/MusicListAdapter.java
@@ -0,0 +1,52 @@
+package com.gouvo.musicplayer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.ViewHolder> {
+
+    ArrayList<Song> songlist;
+    Context context;
+
+    public MusicListAdapter(ArrayList<Song> songlist, Context context) {
+        this.songlist = songlist;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.music_item,parent,false);
+        return new MusicListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Song songData = songlist.get(position);
+        holder.titleTextView.setText(songData.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songlist.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTextView;
+        ImageView iconImageView;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.music_title_text);
+            iconImageView = itemView.findViewById(R.id.icon_view);
+        }
+    }
+}
