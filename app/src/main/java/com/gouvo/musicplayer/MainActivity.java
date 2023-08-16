23d6a6518dc6fb2920fd2f6f7ac26cb1118e1035
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2023-08-16 11:03:21 +0300

    brr

--- app/src/main/java/com/gouvo/musicplayer/MainActivity.java
+++ app/src/main/java/com/gouvo/musicplayer/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         }
                     } else {
                         Uri uri = data.getData();
+
                         loadFileWithThumbnail(uri);
                     }
                 }
